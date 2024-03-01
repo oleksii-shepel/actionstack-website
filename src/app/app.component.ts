c71ff7fbb734791fe489b051339f43d9bb2e12d0
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-03-01 09:28:46 +0200

    initial version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { DocumentationComponent } from './documentation/documentation.component';
+import { FeaturesComponent } from './features/features.component';
+import { IndexComponent } from './index/index.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', pathMatch:'full', component: IndexComponent},
+  {path: 'about', pathMatch:'full', component: AboutComponent},
+  {path: 'documentation', pathMatch:'full', component: DocumentationComponent},
+  {path: 'features', pathMatch:'full', component: FeaturesComponent},
+];
