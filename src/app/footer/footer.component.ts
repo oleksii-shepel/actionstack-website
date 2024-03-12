6a6b738556d5386bf36c90ab515d1c77f437f552
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-03-12 13:46:56 +0200

    added paragraphs to documentation

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  hover: boolean = false;
+  word = "ACTIONSTACK";
+  letters: string[] = [];
 
+  ngOnInit() {
+    this.letters = this.word.split('');
+  }
 }
