6c4e4cdd2a3987281ab585037b9feba6c2b2798d
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-03-12 19:19:30 +0200

    added gists

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/documentation/documentation.component.ts
+++ src/app/documentation/documentation.component.ts
@@ -1,15 +1,29 @@
 import { AfterViewInit, Component } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { asyncScheduler } from 'rxjs';
+import { SanitizeHtmlModule } from '../sanitize.pipe';
 
 @Component({
   selector: 'app-documentation',
   standalone: true,
-  imports: [],
+  imports: [SanitizeHtmlModule],
   templateUrl: './documentation.component.html',
   styleUrl: './documentation.component.scss'
 })
 export class DocumentationComponent implements AfterViewInit {
+  trustedGistCode1!: SafeUrl;
+  trustedGistCode2!: SafeUrl;
+  trustedGistCode3!: SafeUrl;
+  trustedGistCode4!: SafeUrl;
+  trustedGistCode5!: SafeUrl;
+  trustedGistCode6!: SafeUrl;
+
+  constructor(private sanitizer: DomSanitizer) {
+
+  }
 
   ngAfterViewInit() {
+
     window.onscroll = () => {
       var scrollPos = window.scrollY;
       var links = document.getElementsByClassName("content143")[0].getElementsByClassName("button86") as HTMLCollectionOf<HTMLElement>;
@@ -27,5 +41,26 @@ export class DocumentationComponent implements AfterViewInit {
         }
       }
     };
+
+    asyncScheduler.schedule(() => {
+      const gistUrl1 = '<script src="https://gist.github.com/oleksii-shepel/436fa857f68a91c67b10128d30e535ba.js"></script>';
+      const gistUrl2 = '<script src="https://gist.github.com/oleksii-shepel/e640b129da869fb89171e068fe734bb6.js"></script>';
+      const gistUrl3 = '<script src="https://gist.github.com/oleksii-shepel/41d693d51232fd5595cc1653b904c660.js"></script>';
+      const gistUrl4 = '<script src="https://gist.github.com/oleksii-shepel/9f9d797c33634f5d8d4f956f732779f1.js"></script>';
+      const gistUrl5 = '<script src="https://gist.github.com/oleksii-shepel/b06723482bf320d2503a96f41dd8d976.js"></script>';
+      const gistUrl6 = '<script src="https://gist.github.com/oleksii-shepel/87e5ad1f4718335b4e61fc82d2995150.js"></script>';
+
+
+      this.trustedGistCode1 = this.sanitizer.bypassSecurityTrustHtml(gistUrl1);
+      this.trustedGistCode2 = this.sanitizer.bypassSecurityTrustHtml(gistUrl2);
+      this.trustedGistCode3 = this.sanitizer.bypassSecurityTrustHtml(gistUrl3);
+      this.trustedGistCode4 = this.sanitizer.bypassSecurityTrustHtml(gistUrl4);
+      this.trustedGistCode5 = this.sanitizer.bypassSecurityTrustHtml(gistUrl5);
+      this.trustedGistCode6 = this.sanitizer.bypassSecurityTrustHtml(gistUrl6);
+    });
+  }
+
+  adjustHeight(event: any) {
+    event.target.style.height = event.target.contentWindow.document.body.scrollHeight + 'px';
   }
 }
