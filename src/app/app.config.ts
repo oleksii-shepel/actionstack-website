487733b943598fd8ad1723396f0323b1f65c8ec2
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-03-15 11:15:59 +0200

    added angularfire and firebase config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideFirebaseApp } from '@angular/fire/app';
 import { provideRouter } from '@angular/router';
 
+import { initializeApp } from 'firebase/app';
+import { environment } from '../environments/environment.development';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)).ngModule
+  ]
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyDq2UP33VDjeeoFtWCTYnyevNs-F7n-eqM",
+    authDomain: "actionstack-21598.firebaseapp.com",
+    projectId: "actionstack-21598",
+    storageBucket: "actionstack-21598.appspot.com",
+    messagingSenderId: "624756456204",
+    appId: "1:624756456204:web:d24f08a5dbb2979e28a729"
+  }
+};
