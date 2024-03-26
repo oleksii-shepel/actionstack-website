f4a38eabbe7c017c2920a35764e83be0250215b9
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-03-26 13:55:29 +0200

    corrections

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideFirebaseApp, initializeApp as initializeApp_alias } from '@angular/fire/app';
+import { provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideRouter } from '@angular/router';
 
@@ -12,7 +12,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)).ngModule,
-    provideFirestore(() => getFirestore()),
+    provideFirestore(() => getFirestore()).ngModule,
     provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"actionstacklib","appId":"1:588000651620:web:989433d11b8fc9e9b73271","storageBucket":"actionstacklib.appspot.com","apiKey":"AIzaSyDx61H_7SACqt9imitOde8iQA0h2QHbl7o","authDomain":"actionstacklib.firebaseapp.com","messagingSenderId":"588000651620"}))), importProvidersFrom(provideFirestore(() => getFirestore()))
   ]
 };
